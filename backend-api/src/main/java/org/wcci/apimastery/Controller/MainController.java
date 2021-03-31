366e0c398349f33869d240da7c0455bc24eb77ea
Bilen Haile <bhaile26@gmail.com>
2021-03-31 13:49:25 -0400

    change

--- backend-api/src/main/java/org/wcci/apimastery/Controller/MainController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controller/MainController.java
@@ -3,6 +3,7 @@ package org.wcci.apimastery.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.wcci.apimastery.Entities.Album;
 import org.wcci.apimastery.Service.AlbumStorage;
+import org.wcci.apimastery.Service.SongRepository;
 
 @RestController
 public class MainController {
@@ -33,15 +34,27 @@ public class MainController {
     }
 
     @PostMapping("/api/albums")
+    public Iterable<Album> addAlbum(@RequestBody Album albumToAdd) {
+        albumStorage.saveAlbum(albumToAdd);
+        return albumStorage.retrieveAllAlbums();
+    }
+
+    @PutMapping("/api/albums")
     public Iterable<Album> editAlbum(@RequestBody Album albumToEdit) {
-        if (albumToEdit.getId()!= null) {
+        if (albumToEdit.getId() != null) {
             albumStorage.saveAlbum(albumToEdit);
         }
         return albumStorage.retrieveAllAlbums();
     }
+//    @PatchMapping("/api/albums/{albumsId}/Song")
+//    public Albums addArtistToAlbums(@RequestBody Song SongToAdd, @PathVariable Long albumsID){
+//    Album album = albumStorage.retrieveAlbumById(AlbumsId);
+//    Song song = new Song(Albums,SongToAdd.getTitle(), songToAdd.getSummary(), songToAdd.getSummary(), True);
+//    SongRepo.save(Song);
+//    return albumStorage.retrieveAlbumByID(albumsId);
+//}
+
 }
-
-
 
 
 
