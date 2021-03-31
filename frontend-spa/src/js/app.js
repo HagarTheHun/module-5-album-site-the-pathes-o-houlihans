7c958d457136f8ac335576c6ffc45dbe3c1f9d90
Connor <connor@chamfam.net>
2021-03-31 15:36:18 -0400

    displayAlbumView.js is done I think. Dont know how to show it tho

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -8,6 +8,11 @@ import {
     displayHomeView
 } from "./displayHomeView.js"
 import { allAlbumJson } from "./sampleAllAlbums.js";
+
+import {
+    displayAlbumView
+} from "./displayAlbumView.js"
+
 document.querySelector(".container").prepend(createHeader());
 document.querySelector(".container").append(displayHomeView(allAlbumJson));
 document.querySelector(".container").append(createFooter());
