5053c03eb4f07c3f829a5c2fc172adb9fc776515
Connor <connor@chamfam.net>
2021-04-02 11:46:30 -0400

    working on fetch

--- frontend-spa/src/js/app.js
+++ frontend-spa/src/js/app.js
@@ -18,6 +18,14 @@ import {
 } from "./displaySongView.js"
 
  document.querySelector(".container").prepend(createHeader());
- document.querySelector(".container").append(displayHomeView(allAlbumJson));
+//  document.querySelector(".container").append(displayHomeView(allAlbumJson));
+fetch("http://localhost:8080/api/albums", {
+    method: 'GET',
+    mode: 'no-cors'
+})
+.then(response => response.json())
+.then(albums => displayHomeView(albums))
+.then(mainElement => document.querySelector(".container").append(mainElement))
+.catch(ERROR => console.log(ERROR)); 
  document.querySelector(".container").append(createFooter());
 //console.log(allAlbumJson)
